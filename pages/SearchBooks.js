c4ccac8f42cf5c279b2c8bebef98a653b48367d8
waltermatulich <wmatulich231@gmail.com>
2024-03-05 01:10:18 -0800

    updated previous file

--- pages/SearchBooks.js
+++ pages/SearchBooks.js
@@ -1,3 +1,25 @@
-import React from 'react';
-import SearchForm from '../components/SearchForm';
+import React, { useState, useEffect } from 'react';
+import SavedBooks from '../components/SavedBooks';
+import { getSavedBooks } from '../utils/api';
 
+function SavedBooksPage() {
+  const [savedBooks, setSavedBooks] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const books = await getSavedBooks();
+      setSavedBooks(books);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>My Saved Books</h1>
+      <SavedBooks books={savedBooks} />
+    </div>
+  );
+}
+
+export default SavedBooksPage;
